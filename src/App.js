701e5ba02e2b7220576d49dcbaab7dec717de782
GodwinAsuquo <godwinedetauquo@gmail.com>
2022-10-27 14:36:19 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import profilePic from './assets/profile__img.png'
+import { datas } from './data';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ return (
+    <section className='text-center'>
+    <div className='flex justify-center' id='profile__img'>
+        <img src={profilePic} alt="" />
     </div>
-  );
+    <p>Annette Black</p>
+    <div>
+        {datas.map((data, index)=>{
+            const {id, name, link} = data;
+            return <div className='bg-[#EAECF0] w-[90%] md:w-[80%] mx-auto py-4 my-4 rounded-lg text-sm' key={index}>
+                <a href={link} id={id}>{name}</a>
+            </div>
+        })}
+    </div>
+    </section>
+ )
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,28 @@
+
+
+export const datas = [
+    {   id: 'twitter',
+        name: 'Twitter Link',
+        link: 'https://twitter.com/callmeasuquo'
+    },
+    {   id: 'btn__zuri',
+        name: 'Zuri Team',
+        link: 'https://training.zuri.team/'
+    },
+    {   id: 'books',
+        name: 'Zuri Books',
+        link: 'http://books.zuri.team/'
+    },
+    {   id: 'book__python',
+        name: 'Python Books',
+        link: 'https://books.zuri.team/'
+    },
+    {   id: 'pitch',
+        name: 'Background Check for Coders',
+        link: 'https://background.zuri.team/'
+    },
+    {   id: 'book__design',
+        name: 'Design Books',
+        link: 'https://books.zuri.team/design-rules'
+    },
+]
